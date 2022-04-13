71887428b4ce57dd47bb5c24a232593399c78469
Lucas Miranda <lucas.alagoinha@gmail.com>
2022-04-12 22:07:52 -0300

    creating getOneOrganizacao function

--- voluntario/src/main/java/com/br/voluntario/controllers/OrganizacaoController.java
+++ voluntario/src/main/java/com/br/voluntario/controllers/OrganizacaoController.java
@@ -1,6 +1,7 @@
 package com.br.voluntario.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -13,6 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +43,13 @@ public class OrganizacaoController {
         return ResponseEntity.status(HttpStatus.OK).body(organizacaoService.findAll());
     }
 
-    
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneOrganizacao(@PathVariable(value = "id") long id) {
+        Optional<OrganizacaoModel> organizacaoModelOptional = organizacaoService.findById(id);
+        if(!organizacaoModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("A organização não foi localizada");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(organizacaoModelOptional.get());
+    }    
+
 }
