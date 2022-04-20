9f38150082fdf50a33b8f17c18fa91278ba83217
Lucas Miranda <lucas.alagoinha@gmail.com>
2022-04-20 17:27:48 -0300

    creating abstract functions to do a validation

--- voluntario/src/main/java/com/br/voluntario/repositories/VoluntarioRepository.java
+++ voluntario/src/main/java/com/br/voluntario/repositories/VoluntarioRepository.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface VoluntarioRepository extends JpaRepository<VoluntarioModel, Long> {
     
-        
-    
+    boolean existsBycpf(String cpf);   
+    boolean existsByemail(String email);
+    boolean existsBynumero(String numero);
+   
 }
 
