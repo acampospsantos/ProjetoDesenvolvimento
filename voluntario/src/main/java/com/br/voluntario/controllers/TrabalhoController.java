7028436c6a901669ec0d85479898e4c4436b3f44
Lucas Miranda <lucas.alagoinha@gmail.com>
2022-04-21 11:16:14 -0300

    creating post function to trabalhoController

--- voluntario/src/main/java/com/br/voluntario/controllers/TrabalhoController.java
+++ voluntario/src/main/java/com/br/voluntario/controllers/TrabalhoController.java
@@ -1,9 +1,18 @@
 package com.br.voluntario.controllers;
 
+import javax.validation.Valid;
+
+import com.br.voluntario.dtos.TrabalhoDto;
+import com.br.voluntario.models.TrabalhoModel;
 import com.br.voluntario.services.TrabalhoService;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,5 +24,12 @@ public class TrabalhoController {
 
     public TrabalhoController(TrabalhoService trabalhoService) {
         this.trabalhoService = trabalhoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveTrabalho(@RequestBody @Valid TrabalhoDto trabalhoDto) {
+        var trabalhoModel = new TrabalhoModel();
+        BeanUtils.copyProperties(trabalhoDto, trabalhoModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(trabalhoService.save(trabalhoModel));
     }
 }
