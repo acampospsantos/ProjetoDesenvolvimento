d55af22f1926b58ddbbeaad6715574ee3869c2e2
Lucas Miranda <lucas.alagoinha@gmail.com>
2022-04-21 11:30:55 -0300

    creating getOneTrabalho function

--- voluntario/src/main/java/com/br/voluntario/controllers/TrabalhoController.java
+++ voluntario/src/main/java/com/br/voluntario/controllers/TrabalhoController.java
@@ -1,6 +1,7 @@
 package com.br.voluntario.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -14,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +41,11 @@ public class TrabalhoController {
     @GetMapping
     public ResponseEntity<List<TrabalhoModel>> getAllTrabalhos() {
         return ResponseEntity.status(HttpStatus.OK).body(trabalhoService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneTrabalho(@PathVariable(value = "id") long id) {
+        Optional<TrabalhoModel> trabalhoModelOptional = trabalhoService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(trabalhoModelOptional.get());
     }
 }
